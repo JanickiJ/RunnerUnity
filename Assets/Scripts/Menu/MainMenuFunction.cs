12f9f19cda5f49cfc21e13ad568097d5638b47ec
syklzz <agnieszka.szynalik@gmail.com>
2023-04-17 11:45:43 +0200

    added new map sections

--- Assets/Scripts/Menu/MainMenuFunction.cs
+++ Assets/Scripts/Menu/MainMenuFunction.cs
@@ -5,8 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuFunction : MonoBehaviour {
     public GameObject fadeIn;
+    public GameObject playerObject;
 
     void Start() {
+        playerObject.GetComponent<Animator>().Play("Swing Dancing");
         StartCoroutine(StartSequence());
     }
 
